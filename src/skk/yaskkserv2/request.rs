4f388bc81efd0b885a0c226355f0c6aa9eae2d22
Tadashi Watanabe <wac@umiushi.org>
2020-03-08 20:24:00 +0900

    送りが無駄に合成されないようにした

--- src/skk/yaskkserv2/request.rs
+++ src/skk/yaskkserv2/request.rs
@@ -1,60 +1,82 @@
 use crate::skk::yaskkserv2::*;
 
 impl Request {
+    fn is_okuri_midashi_tail(midashi_tail: &[u8]) -> bool {
+        if midashi_tail.len() != 1 {
+            return false;
+        }
+        (midashi_tail[0] >= b'a') && (midashi_tail[0] <= b'z')
+    }
+
+    fn json_str_to_bytes(json: &json::JsonValue) -> Option<&[u8]> {
+        if let Some(s) = json.as_str() {
+            Some(s.as_bytes())
+        } else {
+            None
+        }
+    }
+
     fn get_result(json: json::JsonValue, max_candidates_length: usize) -> Vec<Vec<u8>> {
         let mut result = Vec::new();
         match json.len() {
             2 => {
-                for u_0 in json[0][1].members() {
-                    if let Some(s_0) = u_0.as_str() {
-                        let s_0_bytes = s_0.as_bytes();
-                        for u_1 in json[1][1].members() {
-                            if let Some(s_1) = u_1.as_str() {
-                                let mut v = Vec::from(s_0_bytes);
-                                v.extend_from_slice(s_1.as_bytes());
-                                result.push(v);
+                if let Some(midashi_tail) = Request::json_str_to_bytes(&json[1][0]) {
+                    for u_0 in json[0][1].members() {
+                        if let Some(b_0) = Request::json_str_to_bytes(u_0) {
+                            for u_1 in json[1][1].members() {
+                                if let Some(b_1) = Request::json_str_to_bytes(u_1) {
+                                    let mut v = Vec::from(b_0);
+                                    if !Request::is_okuri_midashi_tail(midashi_tail) {
+                                        v.extend_from_slice(b_1);
+                                    }
+                                    result.push(v);
+                                }
                             }
                         }
                     }
                 }
-            },
+            }
             3 => {
-                for u_0 in json[0][1].members() {
-                    if let Some(s_0) = u_0.as_str() {
-                        let s_0_bytes = s_0.as_bytes();
-                        for u_1 in json[1][1].members() {
-                            if let Some(s_1) = u_1.as_str() {
-                                let s_1_bytes = s_1.as_bytes();
-                                for u_2 in json[2][1].members() {
-                                    if let Some(s_2) = u_2.as_str() {
-                                        let mut v = Vec::from(s_0_bytes);
-                                        v.extend_from_slice(s_1_bytes);
-                                        v.extend_from_slice(s_2.as_bytes());
-                                        result.push(v);
+                if let Some(midashi_tail) = Request::json_str_to_bytes(&json[2][0]) {
+                    for u_0 in json[0][1].members() {
+                        if let Some(b_0) = Request::json_str_to_bytes(u_0) {
+                            for u_1 in json[1][1].members() {
+                                if let Some(b_1) = Request::json_str_to_bytes(u_1) {
+                                    for u_2 in json[2][1].members() {
+                                        if let Some(b_2) = Request::json_str_to_bytes(u_2) {
+                                            let mut v = Vec::from(b_0);
+                                            v.extend_from_slice(b_1);
+                                            if !Request::is_okuri_midashi_tail(midashi_tail) {
+                                                v.extend_from_slice(b_2);
+                                            }
+                                            result.push(v);
+                                        }
                                     }
                                 }
                             }
                         }
                     }
                 }
-            },
+            }
             4 => {
-                for u_0 in json[0][1].members() {
-                    if let Some(s_0) = u_0.as_str() {
-                        let s_0_bytes = s_0.as_bytes();
-                        for u_1 in json[1][1].members() {
-                            if let Some(s_1) = u_1.as_str() {
-                                let s_1_bytes = s_1.as_bytes();
-                                for u_2 in json[2][1].members() {
-                                    if let Some(s_2) = u_2.as_str() {
-                                        let s_2_bytes = s_2.as_bytes();
-                                        for u_3 in json[3][1].members() {
-                                            if let Some(s_3) = u_3.as_str() {
-                                                let mut v = Vec::from(s_0_bytes);
-                                                v.extend_from_slice(s_1_bytes);
-                                                v.extend_from_slice(s_2_bytes);
-                                                v.extend_from_slice(s_3.as_bytes());
-                                                result.push(v);
+                if let Some(midashi_tail) = Request::json_str_to_bytes(&json[3][0]) {
+                    for u_0 in json[0][1].members() {
+                        if let Some(b_0) = Request::json_str_to_bytes(u_0) {
+                            for u_1 in json[1][1].members() {
+                                if let Some(b_1) = Request::json_str_to_bytes(u_1) {
+                                    for u_2 in json[2][1].members() {
+                                        if let Some(b_2) = Request::json_str_to_bytes(u_2) {
+                                            for u_3 in json[3][1].members() {
+                                                if let Some(b_3) = Request::json_str_to_bytes(u_3) {
+                                                    let mut v = Vec::from(b_0);
+                                                    v.extend_from_slice(b_1);
+                                                    v.extend_from_slice(b_2);
+                                                    if !Request::is_okuri_midashi_tail(midashi_tail)
+                                                    {
+                                                        v.extend_from_slice(b_3);
+                                                    }
+                                                    result.push(v);
+                                                }
                                             }
                                         }
                                     }
@@ -63,14 +85,14 @@ impl Request {
                         }
                     }
                 }
-            },
+            }
             _ => {
                 for u in json[0][1].members() {
-                    if let Some(s) = u.as_str() {
-                        result.push(Vec::from(s.as_bytes()));
+                    if let Some(bytes) = Request::json_str_to_bytes(u) {
+                        result.push(Vec::from(bytes));
                     }
                 }
-            },
+            }
         }
         if result.len() > max_candidates_length {
             result[..max_candidates_length].to_vec()
